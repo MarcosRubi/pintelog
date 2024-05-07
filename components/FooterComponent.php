cc88902d2299c8117fa82a0f7f2e8b9bb9ab335d
MarcosRubi <danielhernandez9980@gmail.com>
2024-05-07 09:32:04 -0600

    Actualizando footer

--- components/FooterComponent.php
+++ components/FooterComponent.php
@@ -3,6 +3,6 @@ session_start();
 if (isset($_SESSION['username']) && $_SESSION['logged'] != 'n') {
 ?>
 	<footer>
-		<p>© 2022 Pintelog | creado por <a href="https://mrubi.vercel.app/" target="_blank">Marcos Rubí </a></p>
+		<p>© <?= date('Y') ?> Pintelog | creado por <a href="https://mrubi.dev/" target="_blank">Marcos Rubí </a></p>
 	</footer>
 <?php } ?>
